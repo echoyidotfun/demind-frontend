3dfad1069c0e41196d6cd9c8d5d88cea4ea21439
0xEmiya <tjucssong@gmail.com>
2025-04-14 20:48:05 +0800

    feat(subgraph): adjust subgraph variables

--- lib/services/graphql/client.ts
+++ lib/services/graphql/client.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 
 export const GRAPH_API_URL = process.env.NEXT_PUBLIC_GRAPH_API_URL || "";
 export const GRAPH_API_HEADERS = {
-  Authorization: process.env.NEXT_PUBLIC_GRAPH_API_KEY || "",
+  Authorization: process.env.NEXT_PRIVATE_GRAPH_API_KEY || "",
 };
 
 export const graphqlClient = new GraphQLClient(GRAPH_API_URL, {
